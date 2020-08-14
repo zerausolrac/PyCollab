fcaeb228339cabbdf7366d926e540fe039bd2861
Carlos Suarez <carlos.suarez@blackboard.com>
2020-08-13 23:13:53 -0500

    PyCollab update

--- Config.py
+++ Config.py
@@ -7,10 +7,10 @@
 # etc...
 adict = {
     "verify_certs" : "True",
-    "learn_rest_fqdn" : "lacsedemo.blackboard.com",
-    "learn_rest_key" : "5ac25145-23f2-4365-bdae-6332b14d9b04",
-    "learn_rest_secret" : "lFJlGkKcalm5MKXs5KO8VoVDjyleX9Ux",
-    "collab_key": "lac_lti_prod",
-    "collab_secret": "7BF1F190EDCA51D342AD17096F7ADE7B",
+    "learn_rest_fqdn" : "",
+    "learn_rest_key" : "",
+    "learn_rest_secret" : "",
+    "collab_key": "",
+    "collab_secret": "",
     "collab_base_url": "us.bbcollab.com/collab/api/csa"
 }
