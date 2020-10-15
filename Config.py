dad7beee1a7ad6621b23492d01dacafe5b0ec3ea
Carlos Suarez <carlos.suarez@blackboard.com>
2020-10-14 21:21:47 -0500

    Update Oct 2020

--- Config.py
+++ Config.py
@@ -1,3 +1,13 @@
+'''
+In order to get the Learn credentials, they do not to open a case on behind the blackboard nor email developers@blackboard.com.
+
+They need to go to developer.blackboard.com and register from there to grab the Learn credentials for their application, it is also imperative to remind them that they are creating an application based on your code, so they need to register as a developer.
+
+Now, for Collaborate production they CAN and MUST create a ticket on behind the blackboard requesting their credentials.
+'''
+
+
+
 credenciales = {
     "verify_certs" : "True",
     "learn_rest_fqdn" : "learn URL",
