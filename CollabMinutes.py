17c784f2ece8558a5932354088a968e6e0d9859d
Carlos Suarez <carlos.suarez@blackboard.com>
2020-11-19 13:46:58 -0600

    Update Nov 2020

--- CollabMinutes.py
+++ CollabMinutes.py
@@ -7,8 +7,11 @@ import sys
 
 if __name__ == "__main__":
     param = ut.mainMinutes(sys.argv[1:])
-    if ut.collabMinutes(param[0]) == None:
+    minutes = ut.collabMinutes(param[0])
+    if minutes == None:
         print("The file is not Attendance Report format")
+    else:
+        print("total minutes:", minutes)
 
 
 
